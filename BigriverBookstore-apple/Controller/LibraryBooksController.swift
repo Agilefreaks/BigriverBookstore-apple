51c7281bb414e85d74f577414f50c8a7b10277a1
Cornel Viorel <viorelporumbescu@outlook.com>
2019-03-04 13:59:34 +0200

    Initial commit
    Defined the minimal folder structure and folder groups

---
+++ BigriverBookstore-apple/Controller/LibraryBooksController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryBooksController.swift
+//  BigriverBookstore-apple
+//
+//  Created by Viorel Porumbescu on 04/03/2019.
+//  Copyright © 2019 AgileFreaks. All rights reserved.
+//
+
+import Cocoa
+
+class LibraryBooksController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ BigriverBookstore-apple/Model/Author.swift
@@ -0,0 +1,9 @@
+//
+//  Author.swift
+//  BigriverBookstore-apple
+//
+//  Created by Viorel Porumbescu on 04/03/2019.
+//  Copyright © 2019 AgileFreaks. All rights reserved.
+//
+
+import Foundation
