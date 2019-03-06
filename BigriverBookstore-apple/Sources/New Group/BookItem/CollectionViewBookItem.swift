30a2157c0a48220b25595f64843e411075be3f12
c-Viorel <viorelporumbescu@outlook.com>
2019-03-06 11:44:58 +0200

    Load book cover image async

--- BigriverBookstore-apple/Sources/New Group/BookItem/CollectionViewBookItem.swift
+++ BigriverBookstore-apple/Sources/New Group/BookItem/CollectionViewBookItem.swift
@@ -9,15 +9,32 @@
 import Cocoa
 
 class CollectionViewBookItem: NSCollectionViewItem {
-    @IBOutlet weak var bookTitleTextField:NSTextField!
-    @IBOutlet weak var authorNameTextField:NSTextField!
-    @IBOutlet weak var bookCoverImageView:NSImageView!
+    @IBOutlet var bookTitleTextField: NSTextField!
+    @IBOutlet var authorNameTextField: NSTextField!
+    @IBOutlet var bookCoverImageView: NSImageView!
     
-    var bookInfo:Book? {
-        didSet{
+    var bookInfo: Book? {
+        didSet {
             if let book = bookInfo {
                 bookTitleTextField.stringValue = book.title ?? "No title"
                 authorNameTextField.stringValue = book.author?.name ?? "No name"
+                
+                guard let photoCollection = bookInfo?.photos, photoCollection.count > 0, let imageURL = (bookInfo?.photos?[0] as? Photo?)??.uri else {
+                    return
+                }
+                loadImage(from: imageURL)
+            }
+        }
+    }
+    
+    private func loadImage(from url: URL) {
+
+        DispatchQueue.global().async {
+            let data = try? Data(contentsOf: url) // make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
+            if let data = data {
+                DispatchQueue.main.async {
+                    self.bookCoverImageView.image = NSImage(data: data)
+                }
             }
         }
     }
@@ -26,5 +43,4 @@ class CollectionViewBookItem: NSCollectionViewItem {
         super.viewDidLoad()
         // Do view setup here.
     }
-    
 }
