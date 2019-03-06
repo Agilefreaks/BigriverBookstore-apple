95d1c529a47232f02c9a42f8d514f26ede8879e6
c-Viorel <viorelporumbescu@outlook.com>
2019-03-06 16:47:49 +0200

    Change retrieving method to include book cover image resource

---
+++ BigriverBookstore-apple/Sources/Networking/API JSON/Connexion/APIJSONConnexion.swift
@@ -0,0 +1,34 @@
+//
+//  APIConexion.swift
+//  BigriverBookstore-apple
+//
+//  Created by Viorel Porumbescu on 04/03/2019.
+//  Copyright © 2019 AgileFreaks. All rights reserved.
+//
+import Foundation
+import Spine
+
+class APIJSONConnexion: APIService {
+    let spine = Spine(baseURL: NetworkingConstants.baseURL)
+
+    lazy var registerResources: () = {
+        self.spine.registerResource(BookJSON.self)
+        self.spine.registerResource(AuthorJSON.self)
+        self.spine.registerResource(PhotoJSON.self)
+    }()
+
+    func retrieveBookList(completionHandler: (([Book]) -> Void)?) {
+        _ = registerResources
+        var booksQuery = Query(resourceType: BookJSON.self, path: NetworkingConstants.booksPath)
+        booksQuery.paginate(PageBasedPagination(pageNumber: 1, pageSize: 200))
+        booksQuery.include("author")
+        booksQuery.include("photos")
+
+        spine.find(booksQuery).onSuccess { arg0 in
+            let (resources, _, _) = arg0
+            completionHandler?(resources.compactMap({ (Book(jsonBook: $0 as? BookJSON)) }))
+        }.onFailure { error in
+            print(error)
+        }
+    }
+}
