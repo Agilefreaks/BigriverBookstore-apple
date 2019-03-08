64075e185a162544d358e3ebefa46d2481183242
c-Viorel <viorelporumbescu@outlook.com>
2019-03-08 14:06:44 +0200

    Clean up

--- BigriverBookstore-apple/Sources/Controller/MainNavigationViewController.swift
+++ BigriverBookstore-apple/Sources/Controller/MainNavigationViewController.swift
@@ -11,30 +11,38 @@ import Foundation
 class MainNavigationViewController: NSViewController {
     @IBOutlet var previousButton: NSButton!
     @IBOutlet var currenSectionTitleTextField: NSTextField!
-    
+
     private var sectionsNames: [String] = [""]
+    private var shouldHidepreviousButton: Bool {
+        return children.count == 2
+    }
+
+    private var canGoBack: Bool {
+        return self.children.count >= 2
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.previousButton.isHidden = true
         self.currenSectionTitleTextField.stringValue = ""
     }
-    
+
     @IBAction func goBackInNavigationStack(_ sender: Any) {
-        self.previousButton.animator().isHidden = children.count == 2
+        self.previousButton.animator().isHidden = shouldHidepreviousButton
         _ = self.sectionsNames.popLast()
         self.currenSectionTitleTextField.animator().stringValue = self.sectionsNames.last ?? ""
-        if self.children.count >= 2 {
+        if self.canGoBack {
             let index = self.children.count - 1
-            self.transition(from: self.children[index], to: self.children[index - 1], options: .slideRight, completionHandler: {
+            let previousIndex = self.children.count - 2
+            self.transition(from: self.children[index], to: self.children[previousIndex], options: .slideRight, completionHandler: {
                 _ = self.children.popLast()
-                self.previousButton.animator().isHidden = self.children.count == 1
             })
         }
     }
-    
-    func pushNewControllerInNavigationStack(from oldController: NSViewController, to newController: NSViewController, newSectionName: String = "") {
-        self.currenSectionTitleTextField.stringValue = newSectionName
-        self.sectionsNames.append(newSectionName)
+
+    func pushNewControllerInNavigationStack(from oldController: NSViewController, to newController: NSViewController, withSectionName sectionTitle: String = "") {
+        self.currenSectionTitleTextField.stringValue = sectionTitle
+        self.sectionsNames.append(sectionTitle)
         self.addChild(newController)
         self.transition(from: oldController, to: newController, options: .slideLeft, completionHandler: {})
         self.previousButton.isHidden = false
