0e47f3ba418a9d4cf25b13258f4b008a0cbbfd05
wokePanda <florinusc2@gmail.com>
2019-03-11 10:56:48 +0200

    Created Main View Controller
    - Created the Main ViewController that includes a Collection View
    - Created the book Collection View Cell

---
+++ Big River Book Store/Cells/BookCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  BookCollectionViewCell.swift
+//  Big River Book Store
+//
+//  Created by Florin Uscatu on 11/03/2019.
+//  Copyright © 2019 Florin Uscatu. All rights reserved.
+//
+
+import UIKit
+
+class BookCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Big River Book Store/ViewControllers/BookListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Big River Book Store
+//
+//  Created by Florin Uscatu on 11/03/2019.
+//  Copyright © 2019 Florin Uscatu. All rights reserved.
+//
+
+import UIKit
+
+class BookListViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet private weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.delegate = self
+            collectionView.dataSource = self
+        }
+    }
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
+extension BookListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
