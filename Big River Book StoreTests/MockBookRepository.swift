e4ebe8378d2b7261307849e3da2dbb214f327e28
wokePanda <florinusc2@gmail.com>
2019-03-13 14:09:41 +0200

    Adjusted BookDetailViewModelTests to using BookDetail model

--- Big River Book StoreTests/MockBookRepository.swift
+++ Big River Book StoreTests/MockBookRepository.swift
@@ -26,16 +26,29 @@ class MockBookRepository: BookRepositoryProtocol {
         block([book, book, book], nil)
     }
     
-    func get(include: [String], resourceID: String, completion block: @escaping (Book?, Error?) -> Void) {
+    func get(include: [String], resourceID: String, completion block: @escaping (BookDetail?, Error?) -> Void) {
         guard !error else {
             block(nil, CustomError.generalError)
             return
         }
-        let book = buildBook()
+        let book = buildBookDetail()
         block(book, nil)
     }
     
     private func buildBook() -> Book {
-       return Book(id: "42", title: "Title", author: "author", imageUrls: ["www.google.com", "www.facebook.com", "www.twitter.com"])
+       return Book(id: "42",
+                   title: "Title",
+                   author: "author",
+                   imageUrls: ["www.google.com", "www.facebook.com", "www.twitter.com"])
+    }
+    
+    private func buildBookDetail() -> BookDetail {
+        return BookDetail(id: "42",
+                          title: "Title",
+                          datePublished: "1975-12-04",
+                          author: Author(id: "12", name: "Leo"),
+                          store: Store(id: "32", name: "Diverta"),
+                          price: 3.2,
+                          imageUrls: ["www.google.com", "www.facebook.com", "www.twitter.com"])
     }
 }
